dae56d070b372982ada916793166935b9fc31699
Utkarsh Malviya <utkarsh.99.malviya@gmail.com>
2017-08-12 11:34:30 +0530

    Front Complete

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  var pathEls = document.querySelectorAll('path');
+  for (var i = 0; i < pathEls.length; i++) {
+    var pathEl = pathEls[i];
+    var offset = anime.setDashoffset(pathEl);
+    pathEl.setAttribute('stroke-dashoffset', offset);
+    anime({
+      targets: pathEl,
+      strokeDashoffset: [offset, 0],
+      duration: anime.random(1000, 3000),
+      delay: anime.random(0, 2000),
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine',
+      autoplay: true
+    });
+  }
+});
+
+function load(){
+  setTimeout(function(){
+    $(".startup-message").html("");
+    document.getElementsByClassName("startup")[0].style.animationName = "curtain";
+
+  }, 1000);
+}

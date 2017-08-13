984857e75ee396b1940a038c3490153744f7a6e6
Utkarsh Malviya <utkarsh.99.malviya@gmail.com>
2017-08-13 14:27:05 +0530

    Data Recovery Section COmplete

--- js/index.js
+++ js/index.js
@@ -25,4 +25,41 @@ function load(){
     document.getElementsByClassName("startup")[0].style.animationName = "curtain";
 
   }, 1000);
+  $(".data li:eq(0)").addClass("Active");
+}
+
+var type="";
+function showData(type)
+{
+  if(type === 'Desktop')
+  {
+    $(".data li").removeClass("Active");
+    $(".data li:eq(0)").addClass("Active");
+    $("#show-data").html("Desktop Drives Problems can be logical like knocking, drive no detection, PCB burned, Disk dropped, password Locked, HDD drive making noise or It can be a Physical problems when there is No detection, PCB burned, knocking, Bad Sectos, Wrong Capacity, Noise, Fire Flood affected. We can recover data from desktop drives having any problem.");
+  }
+  if(type === 'Laptop')
+  {
+    $(".data li").removeClass("Active");
+    $(".data li:eq(1)").addClass("Active");
+    $("#show-data").html("Laptop Drives are small size hardware as compare to desktop drives, In order to recover of the data special tooling, equipment and handling procedures are required . Laptop hard disk work the same way as Desktop drives work. Laptop Drives Problems can be logical like Partition not accessible, Partition deleted, Formatted, OS re-installed, Virus attack, Ghost restored, OS restored or Physical problems like Knocking, No Detection, PCB burned, Disk dropped, Password locked. We can recover data from laptop drives having any problem.");
+  }
+  if(type === 'Server')
+  {
+    $(".data li").removeClass("Active");
+    $(".data li:eq(2)").addClass("Active");
+    $("#show-data").html("Data recovery from Server. We understand your business enterprise importance, every business need a reliable server for the 24 hours proper working, A little system failure or problem can cause a big damage, so We understand the importance and critical nature of these type of critical problem, issue and failure. We recover data from IDE, SATA, SAS, SCSI, RAID hard disk running on any Operating system. We can back in business within 24-48 hours by recovering your data lost from the malfunctioning servers hard disk. We can recover data from server drives having any problem.");
+  }
+  if(type === 'PD')
+  {
+    $(".data li").removeClass("Active");
+    $(".data li:eq(3)").addClass("Active");
+    $("#show-data").html("Com-Net can recover Data from your Pen Drive / Flash Drive, Memory card, MMC, SD, & Other Memory card with logical and physical issue. Now-a-days every one uses pen drive for temporary data due to portability, capacity and lost in cost feature, But what happend when you lost your data, Don't worry Com-net Solutions will help you to recover you data.");
+  }
+  if(type === 'Cards')
+  {
+    $(".data li").removeClass("Active");
+    $(".data li:eq(4)").addClass("Active");
+    $("#show-data").html("Com-Net can recover Data from your Memory card, MMC, SD, & other memory card with logical and physical issue. Now-a-days every one uses Memory card for Smart mobile, digital camera, and many devices which uses memory card for personal and business small size data because of handy and required storage for small devices, lost of data ( camera pictures, video, files, important data ) is nightmare for anyone, Don't worry Com-net Solutions will help you to recover you data.");
+  }
+  return false;
 }
